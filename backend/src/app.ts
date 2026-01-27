40357af8fb1b0f414f26fd2683f80316ac6935b8
adament23 <sharmaadament23@gmail.com>
2026-01-27 22:29:25 +0530

    feat: integrate Clerk authentication and enhance error handling in the application

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,14 +1,17 @@
 import express from 'express';
+import { clerkMiddleware } from '@clerk/express'
 import authRoutes from './routes/authRoutes';
 import chatRoutes from './routes/chatRoutes';
 import messageRoutes from './routes/messageRoutes';
 import userRoutes from './routes/userRoutes';
+import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
 
 
 app.use(express.json()); // parsing incoming JSON request bodies and making them available under req.body in your route handlers
 
+app.use(clerkMiddleware())
 
 // Health check endpoint
 app.get('/health', (req, res) => {
@@ -20,5 +23,7 @@ app.use("/api/chats", chatRoutes);
 app.use("/api/message", messageRoutes);
 app.use("/api/users", userRoutes);
 
+//error handlers must come after routes and other middleware so they can catch errors passed with next(err) or thrown inside async route handlers
+app.use(errorHandler);
 
 export default  app;
