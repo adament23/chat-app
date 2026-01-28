a8b13b5341cd35122f93bf642eea8b6bd8f43a81
adament23 <sharmaadament23@gmail.com>
2026-01-28 13:28:06 +0530

    feat: enhance chat creation validation and handle participant ID errors

--- backend/src/controllers/chatController.ts
+++ backend/src/controllers/chatController.ts
@@ -1,6 +1,7 @@
 import type { Response, NextFunction } from 'express';
 import type { AuthRequest } from '../middleware/auth';
 import { Chat } from '../models/Chat';
+import { Types } from 'mongoose';
 
 export async function getChats(req: AuthRequest, res: Response, next: NextFunction) {
     try {
@@ -15,7 +16,7 @@ export async function getChats(req: AuthRequest, res: Response, next: NextFuncti
             const otherParticipants = chat.participants.find((p) => p._id.toString() !== userId);
             return {
                 _id: chat._id,
-                participants: otherParticipants,
+                participants: otherParticipants?? null,
                 lastMessage: chat.lastMessage,
                 lastMessageAt: chat.lastMessageAt,
                 createdAt: chat.createdAt,
@@ -32,7 +33,21 @@ export async function getChats(req: AuthRequest, res: Response, next: NextFuncti
 export async function getOrCreateChat(req: AuthRequest, res: Response, next: NextFunction) {
     try {
        const userId = req.userId;
-       const { participantId } = req.params;
+       const {participantId} = req.params;
+
+       if(!participantId || Array.isArray(participantId)) {
+        res.status(400).json({message: 'Participant ID is required'});
+        return;
+       }
+    if(!Types.ObjectId.isValid(participantId)) {
+        res.status(400).json({message: 'Invalid Participant ID'});
+        return;
+    }
+
+       if(participantId === userId) {
+        res.status(400).json({message: 'Cannot create chat with yourself'});
+        return;
+       }
        
        // Check if chat already exists
          let chat = await Chat.findOne({
