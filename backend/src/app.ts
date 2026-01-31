ac5b80d9a92fc8bc4ced9315ff1ab75cdab3ef5e
adament23 <sharmaadament23@gmail.com>
2026-02-01 02:54:03 +0530

    refactor: improve code organization and enhance CORS configuration in app setup

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,38 +1,52 @@
-import express from 'express';
-import path from 'path'
-import { clerkMiddleware } from '@clerk/express'
-import authRoutes from './routes/authRoutes';
-import chatRoutes from './routes/chatRoutes';
-import messageRoutes from './routes/messageRoutes';
-import userRoutes from './routes/userRoutes';
-import { errorHandler } from './middleware/errorHandler';
+import express from "express";
+import path from "path";
+import cors from "cors";
+
+import { clerkMiddleware } from "@clerk/express";
+
+import authRoutes from "./routes/authRoutes";
+import chatRoutes from "./routes/chatRoutes";
+import messageRoutes from "./routes/messageRoutes";
+import userRoutes from "./routes/userRoutes";
+import { errorHandler } from "./middleware/errorHandler";
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:8081", // expo mobile
+  "http://localhost:5173", // vite web devs
+  process.env.FRONTEND_URL!, // production
+].filter(Boolean);
 
-app.use(express.json()); // parsing incoming JSON request bodies and making them available under req.body in your route handlers
+app.use(
+  cors({
+    origin: allowedOrigins,
+    credentials: true, // allow credentials from client (cookies, authorization headers, etc.)
+  })
+);
 
-app.use(clerkMiddleware())
+app.use(express.json()); // parses incoming JSON request bodies and makes them available as req.body in your route handlers
+app.use(clerkMiddleware());
 
-// Health check endpoint
-app.get('/health', (req, res) => {
-  res.json({ status: 'OK', message: 'Service is running' });
+app.get("/health", (req, res) => {
+  res.json({ status: "ok", message: "Server is running" });
 });
 
 app.use("/api/auth", authRoutes);
 app.use("/api/chats", chatRoutes);
-app.use("/api/message", messageRoutes);
+app.use("/api/messages", messageRoutes);
 app.use("/api/users", userRoutes);
 
-//error handlers must come after routes and other middleware so they can catch errors passed with next(err) or thrown inside async route handlers
+// error handlers must come after all the routes and other middlewares so they can catch errors passed with next(err) or thrown inside async handlers.
 app.use(errorHandler);
 
-// Serve frontend in production
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../../web/dist')));
-  app.get('/{*}', (_, res) => {
-    res.sendFile(path.join(__dirname, '../../web/dist', 'index.html'));
+// serve frontend in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../web/dist")));
+
+  app.get("/{*any}", (_, res) => {
+    res.sendFile(path.join(__dirname, "../../web/dist/index.html"));
   });
 }
 
-export default  app;
+export default app;
